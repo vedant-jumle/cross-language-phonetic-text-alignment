cec65beca3b8cf30c7b25fed49bd8cad2e8668b1
VedantJumle <vedantjumle@gmail.com>
2026-03-22 13:59:08 +0100

    update slurm script with correct time

--- slurm_perturb_scripts.sh
+++ slurm_perturb_scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ir_pipeline
 #SBATCH --partition=gpu
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=1
